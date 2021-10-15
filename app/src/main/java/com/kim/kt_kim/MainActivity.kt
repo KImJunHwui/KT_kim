924b6abd5095359e0410ab72351ca31601feeb34
KJH <daeding7@naver.com>
2021-10-15 19:39:29 +0900

    로그 찍어보기

--- app/src/main/java/com/kim/kt_kim/MainActivity.kt
+++ app/src/main/java/com/kim/kt_kim/MainActivity.kt
@@ -2,13 +2,19 @@ package com.kim.kt_kim
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickbtn.setOnClickListener {
+//             코드에 메모 남기기 - 주석
 
+            Log.d("메인화면로그", "클릭용 버튼 눌림")
+        }
     }
 }
